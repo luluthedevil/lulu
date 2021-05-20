503f3e71fb8b60d036736a061bff3e2353e38ffc
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2021-05-20 09:53:39 -0300

    an animation, a scroll up btn and e footer

--- script.js
+++ script.js
@@ -5,12 +5,30 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide-up
+    $('.scroll-up-btn').on('click', function(){
+        $('html, body').animate({scrollTop: 0}, 0);
+        // removing smooth scroll on slide-up button click
+        $('html, body').css("scrollBehavior", "auto");
     });
     // toggle menu/navbar
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+    //typing animation
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
     //owl carousel
     $('.carousel').owlCarousel({
         margin: 20,
